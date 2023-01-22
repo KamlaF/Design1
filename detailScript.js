a5440b146d904195d192e99aa32342a76dcf0bdc
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-01-22 18:24:54 +0100

    REST API

--- detailScript.js
+++ detailScript.js
@@ -1,10 +1,10 @@
-const detailContainer= document.querySelector(".game-detail");
+const detailContainer= document.querySelector(".gameDetail");
 const queryString= document.location.search;
 const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-const url ="https://kamillafagerland.com/neesiteAPI/wp-json/wc/store/products/" + id + "?_embed";
+const url ="https://kamillafagerland.com/neesiteAPI/wp-json/wc/store/products/" + id + "?_embed/";
 
 console.log(id);
 
@@ -29,11 +29,13 @@ async function getProductsDetails(){
     function createHtml(products){  
         
            
-              detailContainer.innerHTML += `<h1>${products.name}</h1>
-              <h2>${products.prices.price}</h2>
-              <div class="productDetail-image" style="background-image:url('${products.images.src}')"></div>
+              detailContainer.innerHTML +=`
+              <h1>${products.name}</h1>
+              <div class="productDetailImage" style="background-image:url('${products.images[0].src}')"></div>
+              <h2>${products.prices.price}$</h2>
+              <p>${products.short_description}</p>
               <a href="/cart.html"><button class="add-cart2">Add to Cart</button></a>
-              <p>${products.short_description}
+              
               
               
              
