cdf9f6a766ef99626b49c2a64a81a6925ada792a
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-01-22 23:06:53 +0100

    spinner

--- apiScript.js
+++ apiScript.js
@@ -1,18 +1,30 @@
 
 baseUrl = "https://kamillafagerland.com/neesiteAPI/wp-json/wc/store/products?_embed"
 const productContainer = document.querySelector(".productsApi");
+const spinner = document.querySelector(".spinner")
+
+
 
 async function getProducts(url){
   const response = await fetch(url);
   const products = await response.json();
+
+  productContainer.innerHTML = "";
+  
   products.forEach(function(product){
+    
+    
     productContainer.innerHTML +=`
+   
+   
     <a href="/detail.html?id=${product.id}" class="productsApi">
     <h2>${product.name}</h2> 
     <img class="product-image" style="background-image:url('${product.images[0].src}')"></img>
     </a> 
     `
   })
+
+  
 
 }
 
--- detailScript.js
+++ detailScript.js
@@ -10,26 +10,27 @@ console.log(id);
 
 console.log(url);
 
+
+
 async function getProductsDetails(){
+    
 
     try{
     const response = await fetch(url);
     const products = await response.json();
-    
-    
-
     console.log(products);
 
     createHtml(products);
-    
+       
 }catch(error)
 {console.log(error);
 }}    
 
+
+
     function createHtml(products){  
         
-           
-              detailContainer.innerHTML +=`
+             detailContainer.innerHTML +=`
               <h1>${products.name}</h1>
               <div class="productDetailImage" style="background-image:url('${products.images[0].src}')"></div>
               <h2>${products.prices.price}$</h2>
